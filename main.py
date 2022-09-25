15d65f2b5e70ec00835bdbfead0c888899c56c55
LuizScar <48998681+LuizScar@users.noreply.github.com>
2022-09-25 13:57:22 -0300

    commit

--- main.py
+++ main.py
@@ -128,3 +128,162 @@ class DeterministicFiniteAutomaton:
       peso = self.__graph[i+1][1]
       graph[vertix][peso] = adj
     return graph
+
+class NonDeterministicFinitAutomaton:
+  def __init__(self, graph, alphabet, initial, accepting, states):
+      self.__graph = graph
+      self.__alphabet = alphabet
+      self.__initial = initial
+      self.__accepting = accepting
+      self.__states = states
+  
+  def tableOfAllStates(self):
+      statesTable = []
+      statesCombination = self.allStatesCombinations()
+      alphabet = self.__alphabet
+      alphabetSize = len(alphabet)
+      statesCombinationSize = len(statesCombination)
+      increment = 0
+
+      for i in range(statesCombinationSize*alphabetSize):
+        statesTable.append([])
+
+      for i in range(statesCombinationSize):
+        for j in range(alphabetSize):
+          for c in range(0, len(self.__graph),2):
+            vertix = self.__graph[c]
+            adj = self.__graph[c+1][0]
+            peso = self.__graph[c+1][1]
+          
+          if vertix in statesCombination[i] and peso == alphabet[j]:
+              statesTable[increment].append(adj)
+          increment += 1
+          
+      return statesTable, alphabet, statesCombination
+
+  def validateWord(self, word):
+      graph = self.tableofValidStates()
+      currentState = self.__initial
+      for letter in word:
+        for peso, adj in graph[currentState].items():
+          if peso == letter:
+            currentState = adj
+          
+      for fState in self.__accepting:
+        if fState in currentState:
+          return True, graph
+      
+      return False, graph
+      
+  def tableofValidStates(self):
+      graph = self.generateGraph()
+      
+      validGraph = {}
+      visited = []
+      queue = [self.__initial]
+      
+      while queue:
+        validGraph[queue[0]] = {}
+        for peso, adj in graph[queue[0]].items():
+          validGraph[queue[0]][peso] = adj
+          if adj not in visited:
+            queue.append(adj)
+      visited.append(queue.pop(0))
+      
+      return validGraph
+          
+          
+  def generateGraph(self):
+      statesTable, alphabet, statesCombination = self.tableOfAllStates()
+      stringStatesCombination = []
+      stringStatesTable = []
+      graph = {}
+
+      for i in range(len(statesCombination)):
+        stringStatesCombination.append("")
+
+      for i in range(len(statesTable)):
+        stringStatesTable.append("")
+
+      for i in range(len(statesCombination)):
+        for j in statesCombination[i]:
+          stringStatesCombination[i] += j
+
+      for i in range(len(statesTable)):
+        for state in statesTable[i]:
+          stringStatesTable[i] += state
+      
+      count = 0
+      for i in range(len(statesCombination)):
+        state = stringStatesCombination[i]
+        graph[state] = {}
+      for letter in alphabet:
+          graph[state][letter] = stringStatesTable[count]
+          count +=1
+      return graph
+
+  def allStatesCombinations(self):
+      statesSize = len(self.__states)
+      combinationStates = []
+      for i in range(statesSize):
+        for combination in combinations(self.__states, i+1):
+          combinationStates.append(combination)        
+      return combinationStates
+
+class ENonDeterministicFiniteAutomaton:
+  def __init__(self, graph, alphabet, initial, accepting, states):
+      self.__graph = graph
+      self.__alphabet = alphabet
+      self.__initial = initial
+      self.__accepting = accepting
+      self.__states = states
+      
+  def eClosure(self):
+      statesTable = []
+      for i in range(len(self.__states)):
+        statesTable.append(self.__states[i])
+        statesTable.append([])
+
+      for i in range(0,len(self.__graph),2):
+        vertix = self.__graph[i]
+        adj = self.__graph[i+1][0]
+        peso = self.__graph[i+1][1]
+      for c in range(0,len(statesTable),2):
+        tableVertix = statesTable[c]
+        if tableVertix == vertix and peso == '$':
+          statesTable[c+1].append(adj)
+      
+      for i in range(0, len(statesTable),2):
+        vertix = statesTable[i]
+        statesTable[i+1].insert(0,vertix)
+      
+      return statesTable
+
+while True:
+  word = input("Digite a palavra: ")
+
+  print("================================")
+  print("""
+      [1]< DFA
+      [2]< NFA
+      [3]< ENFA
+      [4]< Sair
+  """)
+  select = int(input("Escolha uma opção: "))
+
+  print("================================")
+  if select == 1:
+    automaton = DeterministicFiniteAutomaton(transData,alphabet,initial, accepting, states)
+    validWord =  automaton.validateWord(word)
+    print(validWord)
+  elif select == 2:
+    automaton = NonDeterministicFinitAutomaton(transData,alphabet,initial, accepting, states)
+    validWord, graph =  automaton.validateWord(word)
+    print(validWord)
+    print(graph)
+  elif select == 3:
+    automaton = ENonDeterministicFiniteAutomaton(transData,alphabet,initial, accepting, states)
+    eclosure =  automaton.eClosure()
+    print(eclosure)
+  elif select == 4:
+    break
